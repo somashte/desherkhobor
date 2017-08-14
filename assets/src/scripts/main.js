3f87b18659cc81e05252aa474d1f9fee090aadbd
Jobayer Arman <jobayer.arman@gmail.com>
2017-08-14 11:31:26 +0600

    jumpfix class using affix event trigger

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -3,4 +3,17 @@
   $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+  var $siteHeader = $('.site-header');
+  var className = 'affix-on';
+
+  $('.affix-navigation')
+    .on('affixed.bs.affix', function() {
+      $siteHeader.addClass(className);
+    })
+    .on('affix-top.bs.affix', function() {
+      if ($siteHeader.hasClass(className)) {
+        $siteHeader.removeClass(className);
+      }
+    });
 })(jQuery); // Fully reference jQuery after this point.
