231d907d7cf9916465a2aa16b55040392c282be5
Jobayer Arman <jobayer.arman@gmail.com>
2017-08-19 13:06:54 +0600

    widget ready
    * cat dropdown in admin form
    * update properly
    * output news title lists

--- inc/widgets.php
+++ inc/widgets.php
@@ -13,19 +13,9 @@ class DK_Category_Widget extends WP_Widget {
     public function __construct() {
         $widget_ops = array(
             'classname' => 'dk_category_news_widget',
-            'description' => __('Desher Khobor Category News Wideget', 'desher-khobor-unv')
+            'description' => __('Desher Khobor Category News Sidebar Wideget', 'desher-khobor-unv')
         );
-        parent::__construct( 'dk_category_news_widget', 'DK Category News List Widgets', $widget_ops );
-    }
-
-    /**
-     * Outputs the content of the widget
-     *
-     * @param array $args
-     * @param array $instance
-     */
-    public function widget( $args, $instance ) {
-
+        parent::__construct( 'dk_category_news_widget', 'DK Category News Widgets', $widget_ops );
     }
 
     /**
@@ -35,8 +25,8 @@ class DK_Category_Widget extends WP_Widget {
      */
     public function form( $instance ) {
         $title    = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Widget Name', 'desher-khobor-unv' );
-        $number   = ! empty( $instance['number'] ) ? absint( $instance['number'] ) : absint( '5' );
         $category = ! empty( $instance['category'] ) ? $instance['category'] : __( 'Select a category', 'desher-khobor-unv' );
+        $number   = ! empty( $instance['number'] ) ? absint( $instance['number'] ) : absint( '5' );
         ?>
 
         <p>
@@ -46,11 +36,7 @@ class DK_Category_Widget extends WP_Widget {
 
         <p>
             <label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('News Category:', 'desher-khobor-unv'); ?></label>
-            <select id="<?php echo $this->get_field_id('category'); ?>"  name="<?php echo $this->get_field_name('category'); ?>">
-                <?php for($x=1;$x<=10;$x++): ?>
-                <option <?php echo $x == $category ? 'selected="selected"' : '';?> value="<?php echo $x;?>"><?php echo $x; ?></option>
-                <?php endfor;?>
-            </select>
+            <?php wp_dropdown_categories( array( 'show_option_none' =>' ','name' => $this->get_field_name( 'category' ), 'selected' => $category ) ); ?>
         </p>
 
         <p>
@@ -71,7 +57,52 @@ class DK_Category_Widget extends WP_Widget {
      * @param array $old_instance The previous options
      */
     public function update( $new_instance, $old_instance ) {
-        // processes widget options to be saved
+        $instance = $old_instance;
+
+        $instance['title']    = $new_instance['title'];
+        $instance['category'] = $new_instance['category'];
+        $instance['number']   = $new_instance['number'];
+
+        return $instance;
+    }
+
+    /**
+     * Outputs the content of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget( $args, $instance ) {
+        extract( $args );
+
+        $cat_id = $instance['category'];
+
+        $title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Posts') : get_cat_name( $cat_id ), $instance, $this->id_base);
+
+
+        if( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
+            $number = 5;
+
+        $widget_post = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'cat' => $cat_id, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
+
+        echo $before_widget;
+
+        echo $before_title . $title . $after_title;
+
+        if ($widget_post->have_posts() ) :
+            ?>
+            <ul>
+                <?php while( $widget_post->have_posts() ) : $widget_post->the_post(); ?>
+                <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+                <?php endwhile; ?>
+            </ul>
+
+            <?php
+
+        wp_reset_postdata();
+        endif;
+
+        echo $after_widget;
     }
 }
 
